552e24e78ef5bf1228ce37ca85f2cd51cfd45f0d
JohannZAJO <zajzev@aol.com>
2024-06-26 13:56:23 +0200

    ok

--- AddInApiNet6/cs/AddInApiNet6/SwAddInApiExample.cs
+++ AddInApiNet6/cs/AddInApiNet6/SwAddInApiExample.cs
@@ -8,18 +8,18 @@ namespace Xarial.XCad.Examples
 {
     [ComVisible(true)]
     [Guid("53FDB19A-D7B8-459C-9525-B52CF00FEEE4")]
-    public interface ISwAddInApiExampleApi 
+    public interface ISwAddInApiExampleApi
     {
         void SayHello(string name);
     }
 
     [ComVisible(true)]
     [Guid("557BB880-4F74-43C3-8244-60AEF26CB5F2")]
-    [Title("SW AddIn API Example")]
+    [Title("SW AddIn API Example")] // NE tuts nicht warum auch immer kann nicht aktivert werden
     public class SwAddInApiExample : SwAddInEx, ISwAddInApiExampleApi
     {
         [Title("SW AddIn API Example")]
-        public enum Commands_e 
+        public enum Commands_e
         {
             [Title("Say Hello")]
             [Icon(typeof(Resources), nameof(Resources.hello_icon))]
@@ -33,7 +33,7 @@ namespace Xarial.XCad.Examples
 
         private void OnCommandClick(Commands_e spec)
         {
-            switch (spec) 
+            switch (spec)
             {
                 case Commands_e.SayHello:
                     SayHello(Environment.UserName);
